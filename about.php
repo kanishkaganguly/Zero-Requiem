138a77073cbb1c70e210bb0dfdd6163359d062a3
Nimesh Ghelani <nimeshghelani@gmail.com>
2012-08-03 21:22:12 +0530

    about page

---
+++ about.php
@@ -0,0 +1,78 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen"/>
+        <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="jquery-anyslider.css">
+        <title>About - PHOENIX | CONNEXIONS</title>
+        </script>
+    </head>
+    <body>
+        <div id="head">
+            <div id="head_cen">
+                <div id="head_sup" class="head_pad">
+                    <p class="search">
+                    <form name="login" action ="profile.php" method="POST" class="search">
+                        <input type="text" name ="login_name" class="txt" onfocus="if(this.value == 'Email') { this.value = ''; }" value='Email' size="15" />
+                        <input type="password" name ="login_pwd" class="txt" onfocus="if(this.value == 'Password') { this.value = ''; }" value='Password'  size="15" />
+                        <input type="submit" class="btn" value="LOGIN" name="login_submit" />
+                    </form>
+                    </p>
+
+                    <h1 class="logo"><a href="index.html">PHOENIX CONNEXIONS</a></h1>
+
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a class="active" href="register.php">Sign Up</a></li>
+                        <li><a href="advertisement.php">Market</a></li>
+                        <li><a href="about.php">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="content">
+            <div id="content_cen">
+                <div id="content_sup" class="head_pad">
+                    <div id="welcom_pan">
+                        <h2><span>PCONN</span>About</h2>
+                    </div>
+                    <div id="service_pan">
+			A one stop portal for all your college needs.<br /><br />Currently, following services are part of PHOENIX CONNEXIONS : <br />
+			<div id="welcom_pan"><h3><span>Market</span>Portal</h3></div>
+			A platform for students to sell and buy items among themselves.<br />Just register and start advertising your wares to a huge audience.<br />
+			<div id="welcom_pan"><h3><span>Discussion</span>Forum</h3></div>
+			Just login and start talking about your favourite movies, games, technology, gadgets and much more.<br />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="foot">
+            <div id="foot_cen">
+                <h6><a href="index.php">phoenix</a></h6>
+                <center>
+                    <ul>
+                        <li class="space"></li> <li class="space"></li>
+                        <li class="space"></li><li class="space"></li>
+                        <li class="space"></li><li class="space"></li>
+                        <li class="space"></li><li class="space"></li>
+                        <li class="space"></li><li class="space"></li>
+                        <li class="space"></li><li class="space"></li>
+                        <li><a href="index.php">HOME</a></li>
+                        <li class="space">|</li>
+                        <li><a href="about.php">ABOUT</a></li>
+                        <li class="space">|</li>
+                        <li><a href="services.html">SERVICES</a></li>
+                        <li class="space">|</li>
+                        <li><a href="advertisement.html">MARKET</a></li>
+                    </ul>
+                </center>
+                <p>© Phoenix Connection. Designed by: <t title="Kanishka Ganguly">Nightstalker</t> | <t title="Nimesh Ghelani">Sephiroth</t> | <t title="Soham Chatterjee">ElementCode</t></p>
+            </div>
+        </div>
+    </body>
+</html>
